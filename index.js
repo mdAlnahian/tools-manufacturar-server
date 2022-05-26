b313a9bf1e60464e31df27faf1a91c84b7226896
mdAlnahian <muhammedalnahian@gmail.com>
2022-05-27 02:35:58 +0600

    admin created

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const jwt = require("jsonwebtoken");
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -61,8 +62,17 @@ async function run(){
             $set: user,
           };
           const userResult = await userCollection.updateOne(filter , updateDoc , options) ;
-          res.send(userResult);
+          const token = jwt.sign({ email : email } , process.env.ACCESS_TOKEN_SECRET , { expiresIn : '1h'})
+          res.send({userResult , token });
 
+      });
+
+      //to get all user
+      app.get('/user',async(req,res) => {
+           const query = {};
+           const allUser = userCollection.find(query);
+           const users = await allUser.toArray();
+           res.send(users);
       })
 
       //post method for get item from client
@@ -158,6 +168,27 @@ async function run(){
         res.send(result);
       });
       //code completed for updating profile
+
+      //to make admin
+     
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin })
+        })
+
+        app.put('/user/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const updateDoc = {
+                $set: { role: 'admin' },
+            };
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
+
     }
     finally{
 
